7c117187cd8a22cdc3d8acc1effa70abb3194eda
佳 陆 <2783811832@qq.com>
2020-11-11 14:46:45 +0800

    kjfisudfh

--- EduSystem/EduSystem/frm_EduSystem.Designer.cs
+++ EduSystem/EduSystem/frm_EduSystem.Designer.cs
@@ -54,175 +54,196 @@
             // 
             this.lab_StudentInfo.AutoSize = true;
             this.lab_StudentInfo.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lab_StudentInfo.Location = new System.Drawing.Point(12, 31);
+            this.lab_StudentInfo.Location = new System.Drawing.Point(9, 25);
+            this.lab_StudentInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_StudentInfo.Name = "lab_StudentInfo";
-            this.lab_StudentInfo.Size = new System.Drawing.Size(67, 15);
+            this.lab_StudentInfo.Size = new System.Drawing.Size(53, 12);
             this.lab_StudentInfo.TabIndex = 0;
             this.lab_StudentInfo.Text = "学生信息";
             // 
             // lab_StudentNo
             // 
             this.lab_StudentNo.AutoSize = true;
-            this.lab_StudentNo.Location = new System.Drawing.Point(197, 152);
+            this.lab_StudentNo.Location = new System.Drawing.Point(148, 122);
+            this.lab_StudentNo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_StudentNo.Name = "lab_StudentNo";
-            this.lab_StudentNo.Size = new System.Drawing.Size(0, 15);
+            this.lab_StudentNo.Size = new System.Drawing.Size(0, 12);
             this.lab_StudentNo.TabIndex = 1;
             // 
             // lab_SNo
             // 
             this.lab_SNo.AutoSize = true;
-            this.lab_SNo.Location = new System.Drawing.Point(24, 71);
+            this.lab_SNo.Location = new System.Drawing.Point(18, 57);
+            this.lab_SNo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_SNo.Name = "lab_SNo";
-            this.lab_SNo.Size = new System.Drawing.Size(37, 15);
+            this.lab_SNo.Size = new System.Drawing.Size(29, 12);
             this.lab_SNo.TabIndex = 3;
             this.lab_SNo.Text = "学号";
             // 
             // lab_StudentName
             // 
             this.lab_StudentName.AutoSize = true;
-            this.lab_StudentName.Location = new System.Drawing.Point(24, 123);
+            this.lab_StudentName.Location = new System.Drawing.Point(18, 98);
+            this.lab_StudentName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_StudentName.Name = "lab_StudentName";
-            this.lab_StudentName.Size = new System.Drawing.Size(37, 15);
+            this.lab_StudentName.Size = new System.Drawing.Size(29, 12);
             this.lab_StudentName.TabIndex = 4;
             this.lab_StudentName.Text = "姓名";
             // 
             // lab_Image
             // 
             this.lab_Image.AutoSize = true;
-            this.lab_Image.Location = new System.Drawing.Point(24, 332);
+            this.lab_Image.Location = new System.Drawing.Point(18, 266);
+            this.lab_Image.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_Image.Name = "lab_Image";
-            this.lab_Image.Size = new System.Drawing.Size(37, 15);
+            this.lab_Image.Size = new System.Drawing.Size(29, 12);
             this.lab_Image.TabIndex = 5;
             this.lab_Image.Text = "头像";
             // 
             // btn_MyDesk
             // 
-            this.btn_MyDesk.Location = new System.Drawing.Point(240, 71);
+            this.btn_MyDesk.Location = new System.Drawing.Point(180, 57);
+            this.btn_MyDesk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_MyDesk.Name = "btn_MyDesk";
-            this.btn_MyDesk.Size = new System.Drawing.Size(150, 123);
+            this.btn_MyDesk.Size = new System.Drawing.Size(112, 98);
             this.btn_MyDesk.TabIndex = 6;
             this.btn_MyDesk.Text = "我的桌面";
             this.btn_MyDesk.UseVisualStyleBackColor = true;
             // 
             // btn_StudentAchievement
             // 
-            this.btn_StudentAchievement.Location = new System.Drawing.Point(419, 71);
+            this.btn_StudentAchievement.Location = new System.Drawing.Point(314, 57);
+            this.btn_StudentAchievement.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_StudentAchievement.Name = "btn_StudentAchievement";
-            this.btn_StudentAchievement.Size = new System.Drawing.Size(150, 123);
+            this.btn_StudentAchievement.Size = new System.Drawing.Size(112, 98);
             this.btn_StudentAchievement.TabIndex = 7;
             this.btn_StudentAchievement.Text = "学籍成绩";
             this.btn_StudentAchievement.UseVisualStyleBackColor = true;
             // 
             // btn_TrainManage
             // 
-            this.btn_TrainManage.Location = new System.Drawing.Point(599, 71);
+            this.btn_TrainManage.Location = new System.Drawing.Point(449, 57);
+            this.btn_TrainManage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_TrainManage.Name = "btn_TrainManage";
-            this.btn_TrainManage.Size = new System.Drawing.Size(150, 123);
+            this.btn_TrainManage.Size = new System.Drawing.Size(112, 98);
             this.btn_TrainManage.TabIndex = 8;
             this.btn_TrainManage.Text = "培养管理";
             this.btn_TrainManage.UseVisualStyleBackColor = true;
             // 
             // btn_TestRegistration
             // 
-            this.btn_TestRegistration.Location = new System.Drawing.Point(240, 249);
+            this.btn_TestRegistration.Location = new System.Drawing.Point(180, 199);
+            this.btn_TestRegistration.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_TestRegistration.Name = "btn_TestRegistration";
-            this.btn_TestRegistration.Size = new System.Drawing.Size(150, 120);
+            this.btn_TestRegistration.Size = new System.Drawing.Size(112, 96);
             this.btn_TestRegistration.TabIndex = 9;
             this.btn_TestRegistration.Text = "考试报名";
             this.btn_TestRegistration.UseVisualStyleBackColor = true;
             // 
             // btn_PracticalTraining
             // 
-            this.btn_PracticalTraining.Location = new System.Drawing.Point(419, 249);
+            this.btn_PracticalTraining.Location = new System.Drawing.Point(314, 199);
+            this.btn_PracticalTraining.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_PracticalTraining.Name = "btn_PracticalTraining";
-            this.btn_PracticalTraining.Size = new System.Drawing.Size(150, 120);
+            this.btn_PracticalTraining.Size = new System.Drawing.Size(112, 96);
             this.btn_PracticalTraining.TabIndex = 10;
             this.btn_PracticalTraining.Text = "实践环节";
             this.btn_PracticalTraining.UseVisualStyleBackColor = true;
             // 
             // btn_TeachEvaluation
             // 
-            this.btn_TeachEvaluation.Location = new System.Drawing.Point(599, 249);
+            this.btn_TeachEvaluation.Location = new System.Drawing.Point(449, 199);
+            this.btn_TeachEvaluation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_TeachEvaluation.Name = "btn_TeachEvaluation";
-            this.btn_TeachEvaluation.Size = new System.Drawing.Size(150, 120);
+            this.btn_TeachEvaluation.Size = new System.Drawing.Size(112, 96);
             this.btn_TeachEvaluation.TabIndex = 11;
             this.btn_TeachEvaluation.Text = "教学评价";
             this.btn_TeachEvaluation.UseVisualStyleBackColor = true;
             // 
             // txb_StudentNo
             // 
-            this.txb_StudentNo.Location = new System.Drawing.Point(67, 68);
+            this.txb_StudentNo.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.txb_StudentNo.Location = new System.Drawing.Point(50, 54);
+            this.txb_StudentNo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_StudentNo.Name = "txb_StudentNo";
-            this.txb_StudentNo.Size = new System.Drawing.Size(130, 25);
+            this.txb_StudentNo.Size = new System.Drawing.Size(98, 21);
             this.txb_StudentNo.TabIndex = 12;
             // 
             // txb_StudentName
             // 
-            this.txb_StudentName.Location = new System.Drawing.Point(67, 120);
+            this.txb_StudentName.Location = new System.Drawing.Point(50, 96);
+            this.txb_StudentName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_StudentName.Name = "txb_StudentName";
-            this.txb_StudentName.Size = new System.Drawing.Size(130, 25);
+            this.txb_StudentName.Size = new System.Drawing.Size(98, 21);
             this.txb_StudentName.TabIndex = 13;
             // 
             // lab_Gender
             // 
             this.lab_Gender.AutoSize = true;
-            this.lab_Gender.Location = new System.Drawing.Point(24, 179);
+            this.lab_Gender.Location = new System.Drawing.Point(18, 143);
+            this.lab_Gender.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_Gender.Name = "lab_Gender";
-            this.lab_Gender.Size = new System.Drawing.Size(37, 15);
+            this.lab_Gender.Size = new System.Drawing.Size(29, 12);
             this.lab_Gender.TabIndex = 14;
             this.lab_Gender.Text = "性别";
             // 
             // txb_Gender
             // 
-            this.txb_Gender.Location = new System.Drawing.Point(67, 176);
+            this.txb_Gender.Location = new System.Drawing.Point(50, 141);
+            this.txb_Gender.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_Gender.Name = "txb_Gender";
-            this.txb_Gender.Size = new System.Drawing.Size(130, 25);
+            this.txb_Gender.Size = new System.Drawing.Size(98, 21);
             this.txb_Gender.TabIndex = 15;
             // 
             // lab_Class
             // 
             this.lab_Class.AutoSize = true;
-            this.lab_Class.Location = new System.Drawing.Point(24, 233);
+            this.lab_Class.Location = new System.Drawing.Point(18, 186);
+            this.lab_Class.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_Class.Name = "lab_Class";
-            this.lab_Class.Size = new System.Drawing.Size(37, 15);
+            this.lab_Class.Size = new System.Drawing.Size(29, 12);
             this.lab_Class.TabIndex = 16;
             this.lab_Class.Text = "班级";
             // 
             // lab_BirthDate
             // 
             this.lab_BirthDate.AutoSize = true;
-            this.lab_BirthDate.Location = new System.Drawing.Point(24, 282);
+            this.lab_BirthDate.Location = new System.Drawing.Point(18, 226);
+            this.lab_BirthDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab_BirthDate.Name = "lab_BirthDate";
-            this.lab_BirthDate.Size = new System.Drawing.Size(37, 15);
+            this.lab_BirthDate.Size = new System.Drawing.Size(29, 12);
             this.lab_BirthDate.TabIndex = 17;
             this.lab_BirthDate.Text = "生日";
             // 
             // txb_Class
             // 
-            this.txb_Class.Location = new System.Drawing.Point(67, 230);
+            this.txb_Class.Location = new System.Drawing.Point(50, 184);
+            this.txb_Class.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_Class.Name = "txb_Class";
-            this.txb_Class.Size = new System.Drawing.Size(130, 25);
+            this.txb_Class.Size = new System.Drawing.Size(98, 21);
             this.txb_Class.TabIndex = 18;
             // 
             // txb_BirthDate
             // 
-            this.txb_BirthDate.Location = new System.Drawing.Point(67, 279);
+            this.txb_BirthDate.Location = new System.Drawing.Point(50, 223);
+            this.txb_BirthDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_BirthDate.Name = "txb_BirthDate";
-            this.txb_BirthDate.Size = new System.Drawing.Size(130, 25);
+            this.txb_BirthDate.Size = new System.Drawing.Size(98, 21);
             this.txb_BirthDate.TabIndex = 19;
             // 
             // txb_Image
             // 
-            this.txb_Image.Location = new System.Drawing.Point(67, 329);
+            this.txb_Image.Location = new System.Drawing.Point(50, 263);
+            this.txb_Image.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txb_Image.Name = "txb_Image";
-            this.txb_Image.Size = new System.Drawing.Size(130, 25);
+            this.txb_Image.Size = new System.Drawing.Size(98, 21);
             this.txb_Image.TabIndex = 20;
             // 
             // frm_EduSystem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.txb_Image);
             this.Controls.Add(this.txb_BirthDate);
             this.Controls.Add(this.txb_Class);
@@ -243,6 +264,7 @@
             this.Controls.Add(this.lab_SNo);
             this.Controls.Add(this.lab_StudentNo);
             this.Controls.Add(this.lab_StudentInfo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frm_EduSystem";
             this.Text = "教务系统";
             this.ResumeLayout(false);
